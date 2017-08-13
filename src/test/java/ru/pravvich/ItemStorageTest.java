9846c19eb1a8c5b283c241a9e88e5dac8a8a15b7
Pavel <triodjangopiter@yandex.ru>
2017-08-13 10:45:07 +0300

    add test

--- src/test/java/ru/pravvich/ItemStorageTest.java
+++ src/test/java/ru/pravvich/ItemStorageTest.java
@@ -1,5 +1,27 @@
+package ru.pravvich;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ItemStorageTest {
 
+    @Test
+    public void whenItemSaveThenItemContainInDB() {
+        final ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("jdbc-context.xml");
+
+        final ItemStorage storage = context.getBean(ItemStorage.class);
+
+        final Item item = new Item();
+        item.setName("test");
+        item.setDescription("test");
+
+        final Item result = storage.save(item);
+        assertThat(storage.getAll().contains(result), is(true));
+    }
 }
