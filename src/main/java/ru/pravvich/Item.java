bb705f2fd486b3e34d603e993047df3636133274
Pavel <triodjangopiter@yandex.ru>
2017-08-13 10:43:17 +0300

    add model Item

--- src/main/java/ru/pravvich/Item.java
+++ src/main/java/ru/pravvich/Item.java
@@ -3,10 +3,65 @@ package ru.pravvich;
 /**
  * Author : Pavel Ravvich.
  * Created : 13.08.17.
- * <p>
- * Item
  */
 public class Item {
 
+    private int id;
 
+    private String name;
+
+    private String description;
+
+    public Item() {
+    }
+
+    public Item(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Item)) return false;
+
+        Item item = (Item) o;
+
+        if (id != item.id) return false;
+        if (!name.equals(item.name)) return false;
+        return description.equals(item.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + description.hashCode();
+        return result;
+    }
 }
